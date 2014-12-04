bb127fe0639a1f0574539df0e48453ecfde1989b
linuxtea <linuxtea@163.com>
2014-12-04 13:25:18 +0800

    config

---
+++ backend/config/main-local.php
@@ -0,0 +1,24 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'Z0IdnMKz3pcEqKl6r8JaHKdHNWAq256J',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = 'yii\debug\Module';
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii']['class'] = 'yii\gii\Module';
+	$config['modules']['gii']['generators'] = [
+        'kartikgii-crud' => ['class' => 'warrence\kartikgii\crud\Generator'],
+    ];
+}
+
+return $config;
---
+++ backend/config/main.php
@@ -0,0 +1,57 @@
+<?php
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+
+$config = [
+    'id' => 'app-backend',
+    'basePath' => dirname(__DIR__),
+    'controllerNamespace' => 'backend\controllers',
+    'bootstrap' => ['log','gii'],
+    'modules' => [
+		'gridview' => [
+            'class' => 'kartik\grid\Module',
+        ],
+	],
+    'components' => [
+        'user' => [
+            'identityClass' => 'backend\models\Adminuser',
+            'enableAutoLogin' => true,
+        ],
+		'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=127.0.0.1;dbname=wordpress',
+            'username' => 'root',
+            'password' => 'yes',
+            'charset' => 'utf8',
+			'tablePrefix'=>'lt_'
+        ],
+		'response' => [
+            'formatters' => [
+                'pdf' => [
+                    'class' => 'robregonm\pdf\PdfResponseFormatter',
+                ],
+            ]
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+		
+    ],
+	
+    'params' => $params,
+];
+
+return $config;
